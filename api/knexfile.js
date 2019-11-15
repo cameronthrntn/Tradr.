76567a53945f1d6f9466507b7d267ab114f6c940
Cameron Thornton <shubwub@gmail.com>
2019-11-15 10:34:08 +0000

    adds back-end knex configuration

---
+++ api/knexfile.js
@@ -0,0 +1,33 @@
+const { DB_URL } = process.env;
+const ENV = process.env.NODE_ENV || 'development';
+const creds = ENV === 'production' ? null : require('./db/config');
+
+const baseConfig = {
+  client: 'pg',
+  migrations: {
+    directory: './db/migrations'
+  },
+  seeds: {
+    directory: './db/seeds'
+  }
+};
+
+const customConfig = {
+  development: {
+    connection: {
+      database: 'tradr',
+      ...creds
+    }
+  },
+  test: {
+    connection: {
+      database: 'tradr_test',
+      ...creds
+    }
+  },
+  production: {
+    connection: `${DB_URL}?ssl=true`
+  }
+};
+
+module.exports = { ...customConfig[ENV], ...baseConfig };
