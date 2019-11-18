000160f109c17a23e4f026122c7a82366f2de652
Cameron Thornton <shubwub@gmail.com>
2019-11-18 15:32:19 +0000

    adds a query to filter traders by distance from project

--- api/models/traders-models.js
+++ api/models/traders-models.js
@@ -1,15 +1,16 @@
 const { connection } = require('../db/connection');
+const { getDistances } = require('../utils');
 
-exports.selectAllTraders = (
+exports.selectAllTraders = async (
   sort_by = 'score',
   order = 'desc',
   trade,
   rate,
-  distance,
+  distance = 20,
   score,
   project_id
 ) => {
-  return connection
+  let traders = await connection
     .select('*')
     .from('traders')
     .orderBy(sort_by, order)
@@ -17,18 +18,16 @@ exports.selectAllTraders = (
       if (trade) query.where('traders.trade', '=', trade);
       if (rate) query.where('traders.rate', '=', rate);
       if (score) query.where('traders.score', '=', score);
-    })
-    .then(traders => {
-      if (distance) {
-        return connection
-          .select('lat', 'lng')
-          .from('projects')
-          .where('projects.project_id', '=', project_id);
-      }
-    })
-    .then(stuff => {
-      console.log(stuff.project, stuff.traders.length);
     });
+  const project = await connection
+    .select('lat', 'lng')
+    .from('projects')
+    .where('projects.project_id', '=', project_id);
+  traders = getDistances(project[0], traders).filter(
+    trader => trader.distance <= distance
+  );
+
+  return traders;
 };
 
 exports.selectTraderByUsername = username => {
