229b15183fb442e42f94ec723ccb40a300469f08
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-11-18 08:43:56 +0000

    amends test data to adhere to US date format for dob. adds seeding for users and traders

---
+++ api/db/seeds/seeds.js
@@ -0,0 +1,26 @@
+const {
+  imagesData,
+  userData,
+  projectsData,
+  tradersData,
+  projectsTradersData
+} = require('../data/index');
+
+exports.seed = function(knex) {
+  return knex.migrate
+    .rollback()
+    .then(() => knex.migrate.latest())
+    .then(() => {
+      return knex
+        .insert(userData)
+        .into('users')
+        .returning('*');
+    })
+    .then(res => {
+      return knex
+        .insert(tradersData)
+        .into('traders')
+        .returning('*');
+    })
+    .then(res => {});
+};
