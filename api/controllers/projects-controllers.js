2690120107e1507cc3810c038a8cc7a211c4114c
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-11-18 14:16:10 +0000

    adds query handling options to GET traders endpoint

--- api/controllers/projects-controllers.js
+++ api/controllers/projects-controllers.js
@@ -1,7 +1,7 @@
 const { selectAllProjects } = require('../models/projects-models');
 
 exports.getAllProjects = (req, res, next) => {
-  return selectAllProjects()
+  return selectAllProjects(req.query)
     .then(projects => {
       res.status(200).send({ projects });
     })
--- api/models/projects-models.js
+++ api/models/projects-models.js
@@ -1,8 +1,29 @@
 const { connection } = require('../db/connection');
 
-exports.selectAllProjects = () => {
+exports.selectAllProjects = ({
+  sort_by = 'start_date',
+  order = 'asc',
+  username,
+  status,
+  trader
+}) => {
   return connection
     .select('*')
     .from('projects')
-    .returning('*');
+    .orderBy(sort_by, order)
+    .modify(modifier => {
+      if (username) modifier.where('username', '=', username);
+      if (status) modifier.where('status', '=', status);
+      if (trader)
+        modifier
+          .select('*')
+          .from('traders-projects-junction')
+          .join(
+            'projects',
+            'projects.project_id',
+            '=',
+            'traders-projects-junction.project_id'
+          )
+          .where('traders-projects-junction.trader_username', '=', trader);
+    });
 };
